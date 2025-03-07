f33b2b18037f15f3de9f0a1ba98584a4d88b2404
Sandip Shiwakoti <codewithshiwakoti@gmail.com>
2025-03-07 21:01:27 +0545

    feat: integrate sentry

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const { withSentryConfig } = require('@sentry/nextjs');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
@@ -10,4 +12,14 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withSentryConfig(nextConfig, {
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  reactComponentAnnotation: {
+    enabled: true,
+  },
+  disableLogger: true,
+  automaticVercelMonitors: true,
+});
